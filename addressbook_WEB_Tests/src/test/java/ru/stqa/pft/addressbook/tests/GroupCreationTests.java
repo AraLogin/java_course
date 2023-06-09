e1faf52a7234bd4a6c008fc268f69d4f491864b8
AraLogin <aras6264@gmail.com>
2023-06-09 03:22:20 +0300

    Лекция 4.5
    Формирование коллекции

--- addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test  //фикстура - с английского зазим,т.е тестовый метод зажимается между инициализацией и завершением фикстуры
     public void testGroupCreationTests() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+        List<GroupData> before =app.getGroupHelper().getGroupList();
         app.getNavigationHelper().gotoGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before +1);
+        List<GroupData> after =app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() +1);
     }
 
 }
