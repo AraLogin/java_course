750375ce96f5e3c8900f84cc4fc36ea7eab1a899
AraLogin <aras6264@gmail.com>
2023-06-15 01:30:50 +0300

    Лекция 6.6 + задание 12

--- addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,10 +8,13 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -27,20 +31,18 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        File photo = new File("src/test/resources/i.webp");
-        BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/contacts.csv")));
+        //File photo = new File("src/test/resources/i.webp");
+        BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
         String line = reader.readLine();
         while (line != null) {
-            String[] split = line.split(";");
-            list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
-                    .withAddress(split[2]).withEmail(split[3])
-                    .withEmail2(split[4])
-                    .withEmail3(split[5]).withHomePhone(split[6])
-                    .withMobilePhone(split[7]).withWorkPhone(split[8]).withPhoto(photo)});
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
     @Test (dataProvider = "validContacts")
