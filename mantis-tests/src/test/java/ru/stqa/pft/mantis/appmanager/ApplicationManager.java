2bd5cda1311cd2a50900b033cb10c8251f7a3e4b
AraLogin <aras6264@gmail.com>
2023-06-15 17:34:34 +0300

    Добавлен новый модуль для тестов мантис

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,59 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    private String browser;
+    public WebDriver wd;
+    private JavascriptExecutor js;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+
+        String target = System.getProperty("target","local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(Browser.CHROME.browserName())) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(Browser.FIREFOX.browserName())){
+            FirefoxOptions options = new FirefoxOptions();
+            options.setBinary(new FirefoxBinary(new File("/usr/local/bin/firefox")));
+            wd = new FirefoxDriver(options);
+        } else if (browser.equals(Browser.IE.browserName())){
+            wd = new InternetExplorerDriver();
+        }
+        //Изменение времени ожидания для FF на 1сек., т.к он периодически не успевает прогрузить эелементы страницы
+        if (browser.equals(Browser.FIREFOX.browserName())){
+            wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        } else {
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        }
+        wd.get(properties.getProperty("web.baseUrl"));
+
+    }
+    public void logout() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+    public void stop() {
+        wd.quit();
+    }
+}
