3a41d37974bf78c776bec55bbaf7eaf72c177287
AraLogin <aras6264@gmail.com>
2023-06-15 15:23:07 +0300

    Задание №11 (исправление)

--- addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPageTests.java
+++ addressbook_WEB_Tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPageTests.java
@@ -30,49 +30,26 @@ public class ContactPageTests extends TestBase {
         }
     }
     @Test
-    public void testContactPhones(){
+    public void testsContactPage(){
         app.goTo().homePage();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-
         assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+        assertThat(contact.getAddress(),equalTo(contactInfoFromEditForm.getAddress()));
+        
     }
-
-
     private String mergePhones(ContactData contact) {
         return Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(),contact.getWorkPhone())
                 .stream().filter((s) -> !s.equals(""))
                 .map(ContactPageTests::cleaned)
                 .collect(Collectors.joining("\n"));
     }
-
     public static String cleaned(String phone) {
         return phone.replaceAll("\\s","").replaceAll("[-()]","");
     }
-
-    @Test
-    public void testContactEmails(){
-        app.goTo().homePage();
-        ContactData contact = app.contact().all().iterator().next();
-        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-
-        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
-
-    }
-
-
     private String mergeEmails(ContactData contact) {
         return Arrays.asList(contact.getEmail(),contact.getEmail2(),contact.getEmail3())
                 .stream().filter((s) -> !s.equals("")).collect(Collectors.joining("\n"));
-    }
-
-
-    @Test
-    public void testContactAddress() {
-        app.goTo().homePage();
-        ContactData contact = app.contact().all().iterator().next();
-        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-
-        assertThat(contact.getAddress(),equalTo(contactInfoFromEditForm.getAddress()));
     }
 }
